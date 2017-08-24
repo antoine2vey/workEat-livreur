fde5c94bac4e40d49484723bd51a5e967c3e07ce
Pierre Clouet <maxclouet@wanadoo.fr>
2017-08-24 20:53:42 +0200

    Merge

--- components/Places.js
+++ components/Places.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import CommandsGrouped from './CommandsGrouped';
 
 const Places = ({ places, commands }) => (
   <View>
     {places.map(place =>
-      <View>
-        <Text>
+      <View style={styles.placeContainer} >
+        <Text style={styles.placeTitle} >
           {place.name}
         </Text>
-
         <CommandsGrouped place={place} commands={commands} />
       </View>
     )}
   </View>
 );
+
+const styles = StyleSheet.create({
+  placeContainer: {
+    margin: 20,
+  },
+  placeTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+});
 
 export default Places;
